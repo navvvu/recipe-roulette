d1f2de9a373648a2d9be35969a657242de2536f3
NAVYA KOVVURI <navya.kovvuri@gmail.com>
2025-10-11 13:47:48 -0400

    Restored beautiful matki.jpg logo - was working before

--- app.jsx
+++ app.jsx
@@ -146,7 +146,7 @@ function App() {
         <div className="header-row container">
           <div className="brand">
             <div className="logo-container">
-              <div className="cooking-pot-logo"></div>
+              <img src="./images/matki.jpg" alt="Cooking Pot Logo" className="logo-image" />
             </div>
             <div className="brand-text">
               <h1 className="title">food for thought</h1>
@@ -158,7 +158,7 @@ function App() {
 
           <div className="buttons">
             <div className="logo-container right-logo">
-              <div className="cooking-pot-logo"></div>
+              <img src="./images/matki.jpg" alt="Cooking Pot Logo" className="logo-image" />
             </div>
             <a
               className="button ghost"
